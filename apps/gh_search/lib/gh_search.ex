59ed3b5263a72c5ce5cefc4a3a5a2a131b5d0834
Ricardo Leão <ricardo@jaya.tech>
2022-02-10 20:31:32 -0300

    feat: create new gh_search project

---
+++ apps/gh_search/lib/gh_search.ex
@@ -0,0 +1,18 @@
+defmodule GhSearch do
+  @moduledoc """
+  Documentation for `GhSearch`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GhSearch.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/gh_search/test/gh_search_test.exs
@@ -0,0 +1,8 @@
+defmodule GhSearchTest do
+  use ExUnit.Case
+  doctest GhSearch
+
+  test "greets the world" do
+    assert GhSearch.hello() == :world
+  end
+end
